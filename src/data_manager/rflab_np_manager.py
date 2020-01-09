3b8b9d2afa4f91abea3954d72890af8a88af62fb
Anton Vasilev <bysslaev@gmail.com>
2020-01-10 01:04:46 +0300

    Добавлен дата менеджер для набора данных с rf-lab.org

---
+++ src/data_manager/rflab_np_manager.py
@@ -0,0 +1,77 @@
+"""
+  Created by PyCharm.
+  User: antonvasilev <bysslaev@gmail.com>
+  Date: 10/01/2020
+  Time: 00:39
+ """
+import os
+import re
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from src.data_manager.base_manager import BaseManager
+
+
+class RflabNpDataManager(BaseManager):
+    def __init__(self):
+        super().__init__()
+        self.path = '../data/rf-lab/nine_movs_six_sub_split 2'
+        self.sc = MinMaxScaler(feature_range=(0, 1))
+
+    def load(self):
+        """
+        Load the dataset
+        :return:
+        """
+        self.raw_data = self.load_raw(self.path)
+        self.normalize_data = self.normalize_data(self.raw_data)
+        self.result_data = self.normalize_data
+        # self._X = self.result_data
+        # self._y = np.zeros(self._X.shape)
+        return self.result_data
+
+    def normalize_data(self, data):
+        # TODO
+        return data
+
+    def get_hand_gesture_class(self, file_name):
+        index = int(re.sub(r'^\d_', "", file_name).replace('.txt', ''))
+        if index > 0:
+            index = index - 1
+        return index
+
+    def prepare_signal(self, mat, gesture_class):
+        N = mat.shape[0]
+        m = 400
+        i = 0
+        k = 0
+        result = []
+        while i < N:
+            signal = mat[i:i + N]
+            signal = np.append(signal, [gesture_class])
+            i = i + m
+            result.append(signal)
+            k = k + 1
+        result = np.array(result)
+        return result
+
+    def read_signal(self, file_path):
+        return np.fromfile(file_path)
+
+    def load_raw(self, path):
+        files = os.listdir(os.path.abspath(path))
+        files.sort()
+
+        dataset = []
+
+        k = 0
+        for file in files:
+            if not file.endswith(".txt"):
+                continue
+            data = self.read_signal(path + '/' + file)
+            gesture_class = self.get_hand_gesture_class(file)
+            signal = self.prepare_signal(data, gesture_class)
+            dataset = np.append(dataset, signal, axis=0)
+            k = k + 1
+
+        return dataset
