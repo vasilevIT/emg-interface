8f6cad649a5b914e50617b7b3661201d6df967b6
Anton Vasilev <bysslaev@gmail.com>
2022-10-04 00:13:47 +0300

    Something working...

--- rnn_emg_example.py
+++ rnn_emg_example.py
@@ -3,27 +3,35 @@ from src.data_manager.rflab_np_manager import RflabNpDataManager
 import numpy as np
 from keras.utils import to_categorical
 from keras import Sequential
-from keras.layers import Embedding, SimpleRNN, Dense, BatchNormalization
+from keras.layers import Embedding, SimpleRNN, Dense, BatchNormalization, LSTM, Conv1D, Reshape
 from keras.datasets import imdb
 from keras.utils import pad_sequences
 import matplotlib.pyplot as plt
 
+np.set_printoptions(suppress=True)
+
+
 rflab_manager = RflabNpDataManager()
 dataset = rflab_manager.load()
+
 X_train = dataset[2:, 0:-1]
 X_train[np.isnan(X_train)] = 0.
 print('Shape of one row data:')
 print(X_train.shape)
 print(X_train[0].shape)
 print(X_train[0])
-X_train = np.reshape(X_train, (X_train.shape[0],1,400))
+X_train = np.reshape(X_train, (X_train.shape[0], 400, 1))
 print('ReShape of one row data:')
 print(X_train.shape)
 print(X_train[0].shape)
 print(X_train[0])
 
-# row_sums = X_train.sum(axis=1)
-# X_train = X_train / row_sums[:, np.newaxis]
+print('before nornalize')
+print(X_train[0])
+row_sums = X_train.sum(axis=1)
+X_train = X_train / row_sums[:, np.newaxis]
+print('after nornalize')
+print(X_train[0])
 
 Y_train = dataset[2:, -1]
 print('Class labels:')
@@ -31,21 +39,36 @@ print(np.unique(Y_train))
 
 # Convert labels to categorical one-hot encoding
 one_hot_labels = to_categorical(Y_train, num_classes=9)
+print(one_hot_labels.shape)
+print(np.unique(one_hot_labels))
 
 model = Sequential()
-input_dim = 400
-# model.add(Embedding(input_dim=input_dim, output_dim=64))
-model.add(SimpleRNN(64, input_shape=(1,400)))
+num_classes = 9
+time_series = X_train.shape[1]
+# num_features = X_train.shape[2]
+# model.add(Embedding(input_dim=input_dim, output_dim=64, input_length=400))
+model.add(Reshape((400, 1), input_shape=(400,)))
+# model.add(Conv1D(filters=400, kernel_size=5, activation='relu', input_shape=(None, 400, 1)))
+model.add(SimpleRNN(6, input_shape=(None, 400, 1)))
 model.add(BatchNormalization())
-model.add(Dense(9, activation='softmax'))
+# model.add(Dense(9, activation='sigmoid'))
 
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop', metrics=['accuracy'])
 # Train the model, iterating on the data in batches of 32 samples
-model.fit(X_train, one_hot_labels, epochs=10, batch_size=32, validation_split=0.5)
+model.fit(X_train, one_hot_labels, epochs=10, batch_size=65, validation_split=0.3)
 y_pred = model.predict(X_train[0:1])
 # print(X_train[0:1])
-print('X_pred')
-print(X_train[0:1])
+# print('X_pred')
+# print(X_train[0:1])
 print('y_pred')
 print(y_pred)
+
+plt.plot(X_train[54][0],
+         label='Сигнал')
+plt.xlabel('Время')
+plt.ylabel('Импульс')
+plt.legend()
+plt.savefig('emg_example.png')
