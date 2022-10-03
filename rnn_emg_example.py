13e7a25881a853950b0e8a79a256f38c6af36147
Anton Vasilev <bysslaev@gmail.com>
2022-10-04 00:26:52 +0300

    LSTM example

--- rnn_emg_example.py
+++ rnn_emg_example.py
@@ -3,7 +3,7 @@ from src.data_manager.rflab_np_manager import RflabNpDataManager
 import numpy as np
 from keras.utils import to_categorical
 from keras import Sequential
-from keras.layers import Embedding, SimpleRNN, Dense, BatchNormalization, LSTM, Conv1D, Reshape
+from keras.layers import Embedding, SimpleRNN, Dense, BatchNormalization, LSTM, Conv1D, Reshape, Dropout
 from keras.datasets import imdb
 from keras.utils import pad_sequences
 import matplotlib.pyplot as plt
@@ -20,7 +20,7 @@ print('Shape of one row data:')
 print(X_train.shape)
 print(X_train[0].shape)
 print(X_train[0])
-X_train = np.reshape(X_train, (X_train.shape[0], 400, 1))
+# X_train = np.reshape(X_train, (X_train.shape[0], 400, 1))
 print('ReShape of one row data:')
 print(X_train.shape)
 print(X_train[0].shape)
@@ -45,20 +45,25 @@ print(np.unique(one_hot_labels))
 model = Sequential()
 num_classes = 9
 time_series = X_train.shape[1]
-# num_features = X_train.shape[2]
-# model.add(Embedding(input_dim=input_dim, output_dim=64, input_length=400))
 model.add(Reshape((400, 1), input_shape=(400,)))
-# model.add(Conv1D(filters=400, kernel_size=5, activation='relu', input_shape=(None, 400, 1)))
-model.add(SimpleRNN(6, input_shape=(None, 400, 1)))
-model.add(BatchNormalization())
-# model.add(Dense(9, activation='sigmoid'))
+model.add(LSTM(64, input_shape=(None, 400, 1), return_sequences=True))
+model.add(LSTM(32, return_sequences=True))
+model.add(LSTM(8))
+# model.add(Dense(1024, input_dim=400, activation='relu'))
+# model.add(Dropout(0.1))
+# model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.1))
+# model.add(BatchNormalization())
+# model.add(Dense(256, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dropout(0.1))
 
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop', metrics=['accuracy'])
+              optimizer='adam', metrics=['accuracy'])
 # Train the model, iterating on the data in batches of 32 samples
-model.fit(X_train, one_hot_labels, epochs=10, batch_size=65, validation_split=0.3)
+model.fit(X_train, one_hot_labels, epochs=50, batch_size=65, validation_split=0.3)
 y_pred = model.predict(X_train[0:1])
 # print(X_train[0:1])
 # print('X_pred')
@@ -66,7 +71,7 @@ y_pred = model.predict(X_train[0:1])
 print('y_pred')
 print(y_pred)
 
-plt.plot(X_train[54][0],
+plt.plot(X_train[140],
          label='Сигнал')
 plt.xlabel('Время')
 plt.ylabel('Импульс')
